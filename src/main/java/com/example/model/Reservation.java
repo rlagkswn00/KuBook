0d10787b03730049d9d74dd54094cebde38a1182
kwiyoon <43175569+kwiyoon@users.noreply.github.com>
2024-06-03 11:24:47 +0900

    feat(Reservation) : add lombok annotation

--- src/main/java/com/example/model/Reservation.java
+++ src/main/java/com/example/model/Reservation.java
@@ -1,13 +1,17 @@
 package com.example.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation implements Model{
     public String name;
     public String room;
