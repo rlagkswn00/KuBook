2c23620ee3255eda92a8f769e8ba1e5aa377b029
Han Ju Kim <hjkim2902@gmail.com>
2024-06-08 01:18:32 +0900

    feat : toString 추가

--- src/main/java/com/example/model/Kcube.java
+++ src/main/java/com/example/model/Kcube.java
@@ -1,11 +1,15 @@
 package com.example.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-public class Kcube {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Kcube implements Model {
     public String name;
     public String room;
     public String max;
@@ -14,8 +18,7 @@ public class Kcube {
         try {
             int maxNum = Integer.parseInt(max);
             int roomNum = Integer.parseInt(room);
-            if (maxNum > 20 || maxNum < 1
-                    || roomNum > 3 || roomNum < 1) {
+            if (maxNum > 20 || maxNum < 1) {
                 throw new Exception();
             }
         } catch (Exception e) {
@@ -28,12 +31,16 @@ public class Kcube {
                 .build();
     }
 
-    public static Kcube fromFile(String[] strings){
-        if(strings.length != 3){
+    public static Kcube fromFile(String[] strings) {
+        if (strings.length != 3) {
             System.out.println("파일명 혹은 파일형식에 문제가 있습니다 ! (케이큐브)");
             System.exit(1);
         }
         return from(strings[0], strings[1], strings[2]);
     }
 
+    @Override
+    public String toString() {
+        return name + "," + room + "," + max;
+    }
 }
